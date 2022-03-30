a0f7b763727cee07155d2efcb79a73a9aa7b4e88
SCUR0 <joelm04@gmail.com>
2022-03-30 07:46:59 -0700

    First commit

---
+++ LeaderboardMaintenance/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+	    <title>The Jarl's List - Maintenance</title>
+		<meta property="og:title" content="Isengrim - Maintenance" />
+		<meta property="og:type" content="website" />
+		<meta property="og:site_name" content="Isengrim" />
+		<meta property="og:url" content="https://isengrim.org/LeaderboardMaintenance/" />
+	    <meta property="og:image" content="https://isengrim.org/LeaderboardMaintenance/Isenlogosmall.png" />
+		<meta property="og:image:type" content="image/png" />
+		<meta property="og:image:width" content="220" />
+		<meta property="og:image:height" content="220" />
+		<meta property="og:description" content="Isengrim is currently down for maintenance."/>
+		<meta name="description" content="Isengrim is currently down for maintenance." />
+		<meta name="keywords" content="MWO,Mechwarrior,Online,quickplay,leaderboard,stats,spreadsheet,Isengrim,Jarl,leader board,comparison" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "Leader.css" />
+	</head>
+	<body id = "bod">
+	    <div id = "wrapper">
+			<div id = "maindiv">
+				<div id = 'board' style="overflow-x:auto;">
+					<br>
+					<p style="text-align:center">The Jarl's list and Grim Mechs is currently down temporarily. Please check back later.</p>
+					<br>
+					<img src = "viking.jpeg" id = "vikingpic" alt="Viking boat">
+					<br>
+					<br>
+					<br>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ LeaderboardMaintenance/tech.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+	    <title>The Jarl's List - Maintenance</title>
+		<meta property="og:title" content="The Jarl's List - Maintenance" />
+		<meta property="og:type" content="website" />
+		<meta property="og:site_name" content="The Jarl's List; MWO Leaderboards" />
+		<meta property="og:url" content="https://isengrim.org/LeaderboardMaintenance/" />
+	    <meta property="og:image" content="https://isengrim.org/LeaderboardMaintenance/Isenlogosmall.png" />
+		<meta property="og:image:type" content="image/png" />
+		<meta property="og:image:width" content="220" />
+		<meta property="og:image:height" content="220" />
+		<meta property="og:description" content="The Jarl's list is currently down for maintenance."/>
+		<meta name="description" content="The Jarl's list is currently down for maintenance." />
+		<meta name="keywords" content="MWO,Mechwarrior,Online,quickplay,leaderboard,stats,spreadsheet,Isengrim,Jarl,leader board,comparison" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "Leader.css" />
+	</head>
+	<body id = "bod">
+	    <div id = "wrapper">
+			<div id = "headerbg">
+				<div id = "header">
+					<img src = "Isenlogosmall.png" class = "logo" id = "sublogo" alt="Viking Shield">
+					<h1 id = "subtle">The Jarl's List</h1>
+				</div>
+			</div>
+			<div id = "maindiv">
+				<div id = 'board' style="overflow-x:auto;">
+					<br>
+					<p style="text-align:center">The Jarl's list is currently down due to technical issues. :(</p>
+					<br>
+					<img src = "viking.jpeg" id = "vikingpic" alt="Viking boat">
+					<br>
+					<br>
+					<br>
+				<div id = "nav">
+				</div>
+				</div>
+			</div>
+			
+			<div id="info">
+				<div id="infotext">
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
